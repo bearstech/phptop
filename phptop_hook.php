22c0831559115ec00a916f829ebb7f770559259d
Vincent Caron <vcaron@bearstech.com>
2009-11-13 13:13:52 +0000

    Releasing 0.3.1 - \n in error.log bugfix + more robust parsing

--- phptop_hook.php
+++ phptop_hook.php
@@ -50,7 +50,7 @@ function _phptop_fini() {
   $uri   = $_SERVER['REQUEST_URI'];
   $self  = $vhost != '' ? "$proto://$vhost$uri" : $_SERVER['SCRIPT_FILENAME'];
 
-  $msg = sprintf("phptop %s time:%.6F user:%.6F sys:%.6F mem:%g inc:%g\n", $self, $time, $tusr, $tsys, $mem, $inc);
+  $msg = sprintf("phptop %s time:%.6F user:%.6F sys:%.6F mem:%g inc:%g", $self, $time, $tusr, $tsys, $mem, $inc);
   error_log($msg);
 }
 
