9540b6d153cc2f979a8843572b86167197f9c28d
Vincent Caron <vcaron@bearstech.com>
2010-03-18 23:32:22 +0000

    phptop 0.5 release

--- phptop_hook.php
+++ phptop_hook.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * phptop - Analyse quickly system ressource usage across many PHP queries
- * Copyright (C) 2009 Bearstech - http://bearstech.com/
+ * Copyright (C) 2009,2010 Bearstech - http://bearstech.com/
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
